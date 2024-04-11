dfe03dbb9f26c80a5781f79e19610804e6b4f228
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2024-04-11 10:07:08 -0300

    Add files via upload
    Adicionado verificação de duplicidade de arquivos e renomeação com contador

--- extrair-arquivos-python/main.py
+++ extrair-arquivos-python/main.py
@@ -1,55 +1,73 @@
 import os
 import zipfile
 import shutil
-from dhooks import Webhook
 
-hook = Webhook("https://discord.com/api/webhooks/1196583315394789396/NSeHoScOLSsORZ3JAcuJSBdLCX8jcQm6tOENrqYzo4uUjEVyJ_QLrxbjR-voNSlcrlkW")
-
-#data=input("lalala")
-
-#hook.send("qualquer coisa")
-
-pasta_destino = "C:\\Users\\yuri_\\Downloads\\arquivospdfxml\\"
-pasta_final = "C:\\Users\\yuri_\\Downloads\\arquivosdownload\\"
+origin_folder = "C:\\Users\\yuri_\\Downloads\\arquivosbaixados\\"
+destination_folder = "C:\\Users\\yuri_\\Downloads\\arquivosmovidos\\"
 
 
-if not os.path.exists(pasta_final):
-    os.makedirs(pasta_final)
+if not os.path.exists(destination_folder):
+    os.makedirs(destination_folder)
 
-if not os.path.exists(pasta_destino):
-    os.makedirs(pasta_destino)
-
+if not os.path.exists(origin_folder):
+    os.makedirs(origin_folder)
 
 def processar_arquivos():
+    
+    for arquivo in os.listdir(origin_folder):
+        file_name, ext = os.path.splitext(arquivo)
 
-    for arquivo in os.listdir(pasta_destino):
-        root, ext = os.path.splitext(arquivo)
-        if ext == ".zip" and os.path.exists(pasta_destino + root + ".pdf"):
-            nome_arquivo = root
-            arquivo_zip = zipfile.ZipFile(os.path.join(pasta_destino, arquivo))
-            arquivo_zip.extractall(pasta_destino)
+        if ext == ".zip" and os.path.exists(f'{origin_folder}{file_name}.pdf'):
+            arquivo_zip = zipfile.ZipFile(os.path.join(origin_folder, arquivo))
+            arquivo_zip.extractall(origin_folder)
             um_nome = arquivo_zip.namelist()
             um_nome = um_nome[0]
             arquivo_zip.close()
-            os.remove(os.path.join(pasta_destino, arquivo))
-            enviar_xml(um_nome, nome_arquivo)
-            enviar_pdf(nome_arquivo)
-        else:
-            if ext == ".zip":
-                print("Nao achou o zip e pdf")
+            os.remove(os.path.join(origin_folder, arquivo))
 
-def enviar_xml(um_nome, nome_arquivo):
-    if os.path.exists(pasta_destino + um_nome):
-        shutil.move(pasta_destino + um_nome, pasta_final + nome_arquivo + ".xml")
+            file_count = checkIsUnique(file_name)
+
+            if file_count > 0:
+                file_count += 1
+                enviar_xml(um_nome , f'{nome_arquivo}-{file_count}')
+                enviar_pdf(file_name, f'{file_name}-{file_count}')
+            else: 
+                enviar_xml(um_nome, file_name)
+                enviar_pdf(file_name)
+        elif(ext in "zip"):
+            print("Nao achou o zip e pdf")
+
+def checkIsUnique(file_name):
+    sameItemCount = 0
+    global nome_arquivo
+    for nome_arquivo in os.listdir(destination_folder):
+        if(file_name in nome_arquivo and 'xml' in nome_arquivo):
+            sameItemCount += 1
+            nome_arquivo = file_name
+            print(nome_arquivo)
+
+    return sameItemCount
+
+def enviar_xml(um_nome, file_name):
+    if os.path.exists(f'{origin_folder}{um_nome}'):
+        shutil.move(f'{origin_folder}{um_nome}', f'{destination_folder}{file_name}.xml')
+        print("moveu o xml")
     else:
-        hook.send("Nao achou o XML")
+        print("Nao achou o XML")
 
-def enviar_pdf(nome_arquivo):
-    if os.path.exists(pasta_destino + nome_arquivo + ".pdf"):       
-        shutil.move(pasta_destino + nome_arquivo + ".pdf", pasta_final)
+def enviar_pdf(um_nome, file_name = None):
+    if os.path.exists(f'{origin_folder}{um_nome}.pdf'):       
+
+        if(file_name):
+            shutil.move(f'{origin_folder}{um_nome}.pdf', f'{destination_folder}{file_name}.pdf')
+        else: 
+            shutil.move(f'{origin_folder}{um_nome}.pdf', f'{destination_folder}{um_nome}.pdf')
+
         print("moveu o pdf")
     else:
-        hook.send("Nao achou o PDF")
-
+        print("Nao achou o PDF")
+        
+        
+    
 processar_arquivos()
 
